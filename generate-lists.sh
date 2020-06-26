3add18a2e67d6ad04dc49eb910c5875a957c9c5c
Silas Ferraciolli Corrêa <silascofc@gmail.com>
2020-06-26 00:34:55 -0300

    Adding...

--- generate-lists.sh
+++ generate-lists.sh
@@ -1,56 +1,55 @@
-# Definindo o diretorio temporario onde será feito o trabalho
-temp_dir=/dados/ProgramFiles/dnsmasq
+echo 'Criando um diretório de trabalho temporário'
+TEMP_DIR=$(mktemp -p $(pwd) -d)
 
 # Preparando terreno para jogar as blacklists no diretorio do serviço
 dnsmasqd_path=/etc/dnsmasq.d/
 sudo mkdir -p $dnsmasqd_path
-sudo ln -sf $temp_dir/blacklist.conf $dnsmasqd_path
-
-# URLs das blacklists
-stevenblack_url='https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn/hosts'
-energized_url='https://block.energized.pro/unified/formats/domains.txt'
-oisd_url='https://dbl.oisd.nl'
+sudo ln -sf dnsmasq.conf $dnsmasqd_path
 
 clear
-echo 'Baixando as listas...'
-wget $stevenblack_url -qO $temp_dir/stevenblack-fakenews-gambling-porn.conf
-wget $energized_url -qO $temp_dir/energized-unified-dbl.conf
-wget $oisd_url -qO $temp_dir/oisd-dbl.conf
+echo 'Baixando as listas de:'
+rm -f $TEMP_DIR/blacklist
+for f in $(cat url-sources.txt | grep -v '#'); do
+    echo '    -> '$f
+    wget $f -qO - >> $TEMP_DIR/blacklist
+done
 
-echo 'Gerando a lista temporária...'
-cat $temp_dir/{stevenblack-fakenews-gambling-porn.conf,energized-unified-dbl.conf,oisd-dbl.conf} > $temp_dir/temp.conf
+echo 'Removendo:'
+echo '    -> linhas comentadas...'
+sed -i 's/\#.*//g' $TEMP_DIR/blacklist
+echo '    -> linhas com 127.0.0.1 no início...'
+sed -i '/^127.0.0.1\ /d' $TEMP_DIR/blacklist
+echo '    -> linhas com 255.255.255.255...'
+sed -i '/^255.255.255.255\ /d' $TEMP_DIR/blacklist
+echo '    -> linhas com endereço IPV6...'
+sed -i '/.*\:\:.*/d' $TEMP_DIR/blacklist
 
-echo 'Removendo linhas comentadas...'
-sed -i 's/\#.*//g' $temp_dir/temp.conf
-echo 'Removendo linhas com 127.0.0.1 no início...'
-sed -i '/^127.0.0.1\ /d' $temp_dir/temp.conf
-echo 'Removendo linhas com 255.255.255.255...'
-sed -i '/^255.255.255.255\ /d' $temp_dir/temp.conf
-echo 'Removendo linhas com endereço IPV6...'
-sed -i '/.*\:\:.*/d' $temp_dir/temp.conf
+echo '    -> a coluna 0.0.0.0 de todas as linhas com 2 colunas...'
+sed -i 's/0.0.0.0\ //g' $TEMP_DIR/blacklist
+echo '    -> 0.0.0.0 da blacklist...'
+sed -i '/^0.0.0.0$/d' $TEMP_DIR/blacklist
 
-echo 'Removendo a coluna 0.0.0.0 de todas as linhas com 2 colunas...'
-sed -i 's/0.0.0.0\ //g' $temp_dir/temp.conf
-echo 'Removendo 0.0.0.0 da blacklist...'
-sed -i '/^0.0.0.0$/d' $temp_dir/temp.conf
+echo '    -> todos os espaços...'
+sed -i 's/\s//g' $TEMP_DIR/blacklist
+echo '    -> linhas brancas...'
+sed -i '/^$/d' $TEMP_DIR/blacklist
 
-echo 'Removendo todos os espaços...'
-sed -i 's/\s//g' $temp_dir/temp.conf
-echo 'Removendo linhas brancas...'
-sed -i '/^$/d' $temp_dir/temp.conf
+echo 'Gerando lista base (domínios) ordenada e sem duplicatas...'
+sort $TEMP_DIR/blacklist | uniq > domains.txt
 
-echo 'Ordenando e excluindo duplicatas para formato domínios...'
-sort temp.conf | uniq > domains.txt
+echo 'Copiando lista base para as dos demais formatos...'
+cat domains.txt | tee hosts.txt | tee dnsmasq.conf | tee dnsmasq-ipv6.conf > /dev/null
 
-echo 'Incluindo no início de cada linha address=/ ...'
-sed -i 's/^/address=\//g' $temp_dir/temp.conf
-echo 'Incluindo no final de cada linha o 0.0.0.0 ...'
-sed -i 's/$/\/0.0.0.0/g' $temp_dir/temp.conf
+echo 'Gerando lista hosts.txt ...'
+sed -i 's/^/0.0.0.0\ /g' hosts.txt
 
-echo 'Comentando linhas de domínios com -- (inválido)'
-sed -ri 's/(.*--.*)/\#\1/g' $temp_dir/temp.conf
+echo 'Gerando lista dnsmas.conf e dnsmasq-ipv6.conf ...'
+sed -i 's/^/address=\//g' {dnsmasq.conf,dnsmasq-ipv6.conf}
+sed -i 's/$/\/0.0.0.0/g' dnsmasq.conf
+sed -i 's/$/\/::1/g' dnsmasq-ipv6.conf
+sed -ri 's/(.*--.*)/\#\1/g' {dnsmasq.conf,dnsmasq-ipv6.conf}
 
-echo 'Ordenando e excluindo duplicatas para formato dnsmasq...'
-sort temp.conf | uniq > dnsmasq.conf
+echo 'Removendo diretório temporário...'
+rm -rf $TEMP_DIR
 
 echo 'PRONTO!'
