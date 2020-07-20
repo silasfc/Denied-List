23c68d16e50c47682bead955a682e5c4edcd1763
Silas Ferraciolli Corrêa <silascofc@gmail.com>
2020-07-20 20:45:32 -0300

    Replaced 'whitelist' occurrences by 'allowedlist'.

--- apply-allowedlist-dnsmasq.sh
+++ apply-allowedlist-dnsmasq.sh
@@ -1,5 +1,5 @@
 CURRENT_DIR=$(pwd -P)
 
-for url in $(cat whitelist.txt | grep -v '#'); do
+for url in $(cat allowedlist.txt | grep -v '#'); do
     sed -ri "s/(^server=\/$url\/)/\#\1/" $CURRENT_DIR/dnsmasq.conf
 done
